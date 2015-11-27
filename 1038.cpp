a781d3aa2ce8d4c1dd1b97afeb5b3796027612b1
howexing <568906217@qq.com>
2015-11-27 15:11:09 +0800

    Sort

---
+++ 1038.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+vector<string> col;
+
+bool comp(const string &a, const string &b)
+{
+    int len=(a.size()>b.size())?b.size():a.size();
+    for(int i=0;i<len;++i)
+    {
+        if(a[i]!=b[i])
+            return a[i]<b[i];
+    }
+    if(a.size()>b.size())
+    {
+        for(int i=0;i<a.size()-b.size();++i)
+        {
+            if(a[b.size()+i]!=b[i])
+                return a[b.size()+i]<b[i];
+        }
+    }
+    else if(a.size()<b.size())
+    {
+        for(int i=0;i<b.size()-a.size();++i)
+        {
+            if(a[i]!=b[a.size()+i])
+                return a[i]<b[a.size()+i];
+        }
+    }
+    else
+        return true;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    //cout<<n<<endl;
+    string tmp;
+    for(int i=0;i<n;++i)
+    {
+        cin>>tmp;
+        //cout<<tmp<<endl;
+        col.push_back(tmp);
+    }
+    sort(col.begin(),col.end(),comp);
+    bool nonzero=false;
+    for(int i=0;i<n;++i)
+    {
+        if(!nonzero)
+        {
+            int j=0;
+            for(;j<col[i].size();++j)
+            {
+                if(col[i][j]!='0')
+                    break;
+            }
+            if(j<col[i].size())
+            {
+                nonzero=true;
+                for(;j<col[i].size();++j)
+                    printf("%c",col[i][j]);
+            }
+        }
+        else
+            printf("%s",col[i].c_str());
+    }
+    printf("\n");
+    return 0;
+}
