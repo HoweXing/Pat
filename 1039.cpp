da18dfedcdefd7179119066b98c4467a9d91dc24
howexing <568906217@qq.com>
2015-11-27 16:09:28 +0800

    Hash

---
+++ 1039.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+map<int,vector<int> > Map;
+set<int>::iterator it;
+
+int Hash(char* s)
+{
+    return (s[0]-'A')*1000+(s[1]-'A')*100+(s[2]-'A')*10+s[3]-'0';
+}
+
+int main()
+{
+    int n,k;
+    scanf("%d %d",&n,&k);
+    string name;
+    char tmp[5];
+    for(int i=0;i<k;++i)
+    {
+        int index,m;
+        scanf("%d %d",&index,&m);
+        for(int j=0;j<m;++j)
+        {
+            scanf("%s",tmp);
+            Map[Hash(tmp)].push_back(index);
+        }
+    }
+    for(int i=0;i<n;++i)
+    {
+        scanf("%s",tmp);
+        int hnm=Hash(tmp);
+        printf("%s %d",tmp,Map[hnm].size());
+        sort(Map[hnm].begin(),Map[hnm].end());
+        int j=0,e=Map[hnm].size();
+        for(int j=0;j<e;++j)
+            printf(" %d",Map[hnm][j]);
+        printf("\n");
+    }
+    return 0;
+}
