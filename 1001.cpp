dcf58556a0d9e451a8d01406ca38859bb869c2b3
howexing <568906217@qq.com>
2015-11-29 17:01:28 +0800

    Format Output

--- 1001.cpp
+++ 1001.cpp
@@ -1,26 +1,24 @@
 #include <stdio.h>
-#include <iostream>
 #include <cmath>
 using namespace std;
-
 int main()
 {
     int a,b;
-    cin>>a>>b;
+    scanf("%d%d",&a,&b);
     a+=b;
     b=a;
     a=abs(a);
-    char sta[12],re[20];
+    char stack[12],re[20];
     int top=-1,rtop=-1;
     do
     {
-        sta[++top]='0'+a%10;
+        stack[++top]='0'+a%10;
         a/=10;
     }while(a>0);
     int cnt=0;
     for(int i=0;i<=top;++i)
     {
-        re[++rtop]=sta[i];
+        re[++rtop]=stack[i];
         ++cnt;
         if(cnt%3==0&&i!=top)
             re[++rtop]=',';
