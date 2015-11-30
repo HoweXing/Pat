f742b1baeb50cf22cf7ac5ea7a647afa89f2fdf5
howexing <568906217@qq.com>
2015-11-30 20:52:09 +0800

    Easy

---
+++ 1011.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+#define NUM 3
+using namespace std;
+
+double games[NUM][NUM];
+
+int Best(double *g)
+{
+    double max=0;
+    int id=-1;
+    for(int i=0;i<NUM;++i)
+    {
+        if(g[i]>max)
+        {
+            max=g[i];
+            id=i;
+        }
+    }
+    return id;
+}
+
+string crt[NUM]={"W","T","L"};
+
+int main()
+{
+    double sum=1;
+    for(int i=0;i<NUM;++i)
+    {
+        for(int j=0;j<NUM;++j)
+            cin>>games[i][j];
+        int id=Best(games[i]);
+        cout<<crt[id]<<"  ";
+        sum*=games[i][id];
+    }
+    sum=sum*0.65;
+    sum-=1;
+    sum*=2;
+    printf("%.2f\n",sum);
+    return 0;
+}
